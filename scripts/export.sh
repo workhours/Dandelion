3d6d31ad08ae5bf6f35f229410d8c4f4f77af3b3
truetime <deng.hui5@gmail.com>
2016-09-30 23:12:18 +0800

    fix bug in shell script

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-
 # this script is for exporting document from collection by specified query string
+# check script syntax on: http://www.shellcheck.net/#
 
 # usage:
-# 	export.bat process
-# 	export.bat process New*
-# 	export.bat process "New Task"
-# 	export.bat C39000
-# 	export.bat C39000 57a83691e94ebb4e2061e3fe
+# . ./export process
+# . ./export process New*
+# . ./export process "New Task"
+# . ./export C39000
+# . ./export C39000 57a83691e94ebb4e2061e3fe
 
-set echo off
-echo Export $1 $2
-if ["$1"=="process"]; then
-	mongoexport -d AZDOC -c $1 -o $1.json -q "{ 'T33510.T33511.T33513' :{$regex: '$2'} }" 
-elif ["%2"==""]; then
-	mongoexport -d AZDOC -c $1 -o $1.json
+# set -x
+echo "Export $1 $2"	
+if [ "$1" = "process" ]; then
+	mongoexport -d AZDOC -c "$1" -o "$1".json -q "{ 'T33510.T33511.T33513' :{\$regex: '$2'} }" 
+elif [ $# = 1 ]; then
+	mongoexport -d AZDOC -c "$1" -o "$1".json
 else 
-	mongoexport -d AZDOC -c $1 -o $1.json -q "{ '_id' :{'$oid': '%2' }}"
+	mongoexport -d AZDOC -c "$1" -o "$1".json -q "{ '_id' :{'\$oid': '$2' }}"
 fi
